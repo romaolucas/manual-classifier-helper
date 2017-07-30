c2c65fc37714c3bb9ac21bcc8e9202f1fa48f725
Lucas Romao <lucas.romao.silva@gmail.com>
2017-07-30 19:10:10 -0300

    creating models for tweet and reviews

---
+++ classification/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Tweet(models.Model): 
+    tweet_text = models.TextField(verbose_name='text', max_length=300)
+    tweet_username = models.CharField(verbose_name='username', max_length=255)
+    tweet_id = models.BigIntegerField(verbose_name='id do tweet')
+
+class Review(models.Model):
+    review = (
+            (1, 'Positivo'),
+            (0, 'Neutro'),
+            (-1, 'Negativo')
+            )
+    tweet = models.ForeignKey('Tweet', on_delete=models.CASCADE)
+    ironic = models.BooleanField(verbose_name='contem ironia', default=False)
+    review = models.IntegerField(verbose_name='Avaliacao', choices=review)
+    
+    def is_ironic(self):
+        return self.ironic
--- mchelper/settings.py
+++ mchelper/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.1.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'classification.apps.ClassificationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
