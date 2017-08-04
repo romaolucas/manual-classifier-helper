41ad78a5e4f2e2a331e945a5b42b07e3b5830ef2
Lucas Romao <lucas.romao.silva@gmail.com>
2017-08-04 18:37:51 -0300

    adding initial view, needs to be finished

--- classification/views.py
+++ classification/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.forms import formset_factory
+from .models import Tweet, Review
+from .forms import ReviewForm
 
-# Create your views here.
+def index(request):
+    return render(request, 'classification/index.html', {})
+
+def review(request, tweets_to_review):
+    ReviewFormset = formset_factory(ReviewForm, extra=tweets_to_review)
+    if request.method == 'POST':
+        review_formset = ReviewFormset(request.POST)
+        for review_form in review_formset:
+            tweet_id = review_form.cleaned_data.get('tweet')
+            review = review_form.cleaned_data.get('review')
+            ironic = review_form.cleaned_data.get('ironic')
+            reviewed_tweet = Tweet.objects.get(pk=tweet_id)
+            user_review = Review.objects.get_or_create(tweet=reviewed_tweet)
+            user_review.review = review
+            user_review.ironic = ironic
+            user_review.save()
+    else:
+        tweets_to_review = Tweet.objects.filter(review__isnull=True)
+        initial_values = [{'tweet': tweet.pk, 'tweet_text': tweet.tweet_text} for tweet in tweets_to_review]
+        review_formset = ReviewFormset(initial=initial_values)
+    context = {'review_formset': review_formset}
+    return render(request, 'classification/review.html', context)
