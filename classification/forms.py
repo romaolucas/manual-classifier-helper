c5f527aaf6305d0f13ab6eb4fd7e7a1f19b54ba0
Lucas Romao <lucas.romao.silva@gmail.com>
2017-08-06 16:10:33 -0300

    adding css class and review template

--- classification/forms.py
+++ classification/forms.py
@@ -6,7 +6,7 @@ class ReviewForm(forms.Form):
             (0, 'Neutro'),
             (-1, 'Negativo')
             )
-    review = forms.ChoiceField(choices=review_choices, required=True, label='Opinião do tweet', widget=forms.RadioSelect(attrs={'class': 'form-control'}))
+    review = forms.ChoiceField(choices=review_choices, required=True, label='Opinião do tweet:', widget=forms.RadioSelect(attrs={'class': 'form-control'}))
     ironic = forms.BooleanField(required=False, label='É irônico?', widget=forms.CheckboxInput(attrs={'class': 'form-control'}))
-    tweet = forms.IntegerField(widget=forms.HiddenInput())
+    tweet = forms.IntegerField(label='Texto: ', widget=forms.HiddenInput())
     tweet_text = forms.CharField(widget=forms.Textarea(attrs={'disabled': 'disabled', 'class': 'form-control'}))
