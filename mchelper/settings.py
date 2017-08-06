bb72ce4be18dbe26eb7a40162aa3f80a30a46f42
Lucas Romao <lucas.romao.silva@gmail.com>
2017-08-06 14:20:21 -0300

    adding templates folder

--- mchelper/settings.py
+++ mchelper/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mchelper.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,5 +124,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
-TEMPLATES_DIR = (os.path.join(BASE_DIR, 'templates/'))
