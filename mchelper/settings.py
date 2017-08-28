4b6916b732b65d206b170fc0fb6a23170a3ff624
Lucas Romao <lucas.romao.silva@gmail.com>
2017-08-28 19:58:51 -0300

    new requirements and import_export app

--- mchelper/settings.py
+++ mchelper/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.1.1']
 
 INSTALLED_APPS = [
     'classification.apps.ClassificationConfig',
+    'import_export',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
