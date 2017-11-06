4f05106ecba21b00bc6ba82181c038341a4adead
Lucas Romão <lucas.romao.silva@gmail.com>
2017-11-06 16:12:44 -0200

    script for csv

---
+++ generate_csv.py
@@ -0,0 +1,14 @@
+import csv
+import codecs
+from classification.models import Tweet, Review
+
+with open('output_clam.csv', mode='w') as output:
+    reviewed_tweets = Tweet.objects.filter(review__isnull=False) \
+            .filter(review__ironic=False)
+    fieldnames = ['username', 'tweet', 'review']
+    writer = csv.DictWriter(output, fieldnames=fieldnames, separator=',')
+    writer.writeheader()
+    for tweet in reviewed_tweets:
+        writer.writerow({'username': tweet.tweet_username, 'text': tweet.tweet_text.encode('utf-8').decode('utf-8'), 'review': tweet.review_set.all()[0].review})
+
+
